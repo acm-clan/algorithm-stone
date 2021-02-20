cff6912344d6aca9dcd294df4dd3030833cb015b
dansen <370283593@qq.com>
2021-02-20 17:42:51 +0800

    update node and edge

--- src/datamap.py
+++ src/datamap.py
@@ -16,10 +16,27 @@ class DataMap:
     def __init__(self, data):
         self.data = data
         self.nodes = []
+        self.last_root_name = ""
         self.parse()
 
-    def print(self):
-        pass
+    def consume_problems(self, pos, node):
+        length = len(self.data)
+        p = ""
+
+        while pos < length:
+            c = self.data[pos]
+            if c == '[':
+                return pos
+            if c >= '0' and c <= '9':
+                p += c
+            elif c == '[':
+                return pos
+            elif len(p) > 0:
+                print("add problem:", p)
+                node.problems.append(int(p))
+                p = ""
+            pos += 1
+        return pos
 
     def consume_name(self, pos, node):
         length = len(self.data)
@@ -34,6 +51,9 @@ class DataMap:
                 node.is_root = not node.name.startswith("-")
                 if not node.is_root:
                     node.name = node.name[1:].strip()
+                    node.parent = self.last_root_name
+                else:
+                    self.last_root_name = node.name
                 return pos + 1
             elif state == 1:
                 name += c
@@ -43,16 +63,22 @@ class DataMap:
     def consume_node(self, pos):
         node = DataMapNode("", [], False, None)
         pos = self.consume_name(pos, node)
+        if pos < 0:
+            return pos
+        
         print("name:", node.name)
-        if pos > 0:
-            self.nodes.append(node)
+
+        pos = self.consume_problems(pos, node)
+        if pos < 0:
+            return pos
+
+        self.nodes.append(node)
         return pos
 
     def parse(self):
         pos = 0
         while pos >= 0:
             pos = self.consume_node(pos)
-            print("consume node:", pos)
         print("parse finished:", self.nodes)
 
 
--- src/main.py
+++ src/main.py
@@ -3,26 +3,40 @@ import os
 from pathlib import Path
 import datamap
 
+
 def get_map(map_file):
     path = Path(os.path.abspath(__file__))
     return os.path.abspath(os.path.join(path.parent.parent, "map", map_file))
+
 
 def get_leetcode_txt():
     f = open(get_map("leetcode.txt"), "r", encoding="utf-8")
     t = f.read()
     return t
 
-m = datamap.DataMap(get_leetcode_txt())
-m.print()
 
 def main():
-    g = Digraph('stones')
-    g.node(name='a',color='red')
-    g.node(name='b',color='blue')
-    g.edge('a','b',color='green')
+    m = datamap.DataMap(get_leetcode_txt())
+    g = Digraph('stones', encoding='utf-8')
+
+    for n in m.nodes:
+        g.node(name=n.name, color='lightgrey', fontname="Microsoft YaHei")
+        if not n.is_root:
+            g.edge(n.parent, n.name)
+
+        # add problem
+        last = ""
+        for p in n.problems:
+            g.node(name=str(p), color='lightgrey', fontname="Microsoft YaHei")
+            if len(last) > 0:
+                g.edge(last, str(p))
+            else:
+                g.edge(n.name, str(p))
+            last = str(p)
+
+    g.render()
     # g.view()
 
+
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
-    
