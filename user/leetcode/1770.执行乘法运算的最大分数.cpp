55489323e8ec3c0abe4c7055d199ae888b9009fe
acm-clan <dansen007@gmail.com>
2021-02-22 21:51:21 +0800

    mv code source

---
+++ user/leetcode/1770.执行乘法运算的最大分数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1770 lang=cpp
+ *
+ * [1770] 执行乘法运算的最大分数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+
+    }
+};
+// @lc code=end
+
