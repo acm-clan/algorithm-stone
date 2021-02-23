3df9ab18e3916f89a0377a379867d6abc02feaa7
dansen <370283593@qq.com>
2021-02-23 15:37:32 +0800

    add 213

---
+++ user/leetcode/118.杨辉三角.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=cpp
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+    }
+};
+// @lc code=end
+
--- user/leetcode/213.打家劫舍-ii.cpp
+++ user/leetcode/213.打家劫舍-ii.cpp
@@ -8,7 +8,34 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)return 0;
+        if(n == 1)return nums[0];
+        if(n == 2)return max(nums[0], nums[1]);
+        int f[n+10];
+        f[0] = nums[0];
+        f[1] = nums[1];
+        // 不让开头和末尾在一起
+        for(int i=2; i<n-1; i++){
+            int m = 0;
+            for(int j=0; j<i-1; j++){
+                m = max(m, f[j]);
+            }
+            f[i] = m + nums[i];
+        }
+        int r1 = max(f[n-2], f[n-3]);
 
+        // 
+        f[2] = nums[2];
+        for(int i=3; i<n; i++){
+            int m = 0;
+            for(int j=1; j<i-1; j++){
+                m = max(m, f[j]);
+            }
+            f[i] = m + nums[i];
+        }
+        int r2 = max(f[n-1], f[n-2]);
+        return max(r1, r2);
     }
 };
 // @lc code=end
