f452f6c774a56ffb8093051d944fb4bce1c78d8d
dansen <370283593@qq.com>
2021-02-25 10:03:34 +0800

    add leetcode 1289

--- user/leetcode/1289.下降路径最小和-ii.cpp
+++ user/leetcode/1289.下降路径最小和-ii.cpp
@@ -4,30 +4,33 @@
  * [1289] 下降路径最小和  II
  */
 
+#include <vector>
+using namespace std;
+
 // @lc code=start
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& g) {
         int n=g.size();
         int f[n][n];
-        int m = INT_MAX;
+        
         for(int j=0; j<n; j++){
             f[0][j]=g[0][j];
         }
         
         for(int i=1; i<n; i++){
             for(int j=0; j<n; j++){
-                int l = j-1;
-                int r = j+1;
-
-                if(l<0 || r>=n){
-                    f[i][j] = INT_MAX/2;
-                }else{
-                    f[i][j] = min(f[i-1][l], f[i-1][r])+g[i][j];
+                f[i][j] = INT_MAX;
+                // 算出每个位置的最小值值
+                for(int k=0; k<n; k++){
+                    if(k!=j){
+                        f[i][j] = min(f[i-1][k]+g[i][j], f[i][j]);
+                    }
                 }
-                printf("f[%d][%d]=%d\n", i, j, f[i][j]);
             }
         }
+
+        int m = INT_MAX;
         for(int j=0; j<n; j++){
             m = min(m, f[n-1][j]);
         }
