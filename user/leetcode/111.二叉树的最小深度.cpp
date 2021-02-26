51960c524cd0c49097cb1f96ce027cfec250678a
dansen <370283593@qq.com>
2021-02-26 10:56:18 +0800

    add 111

---
+++ user/leetcode/111.二叉树的最小深度.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=cpp
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* t) {
+        return 0;
+    }
+};
+// @lc code=end
+
