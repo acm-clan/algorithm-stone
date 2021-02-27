14987415d1206f22f1889ce4e203d58fd4b5eb6b
TowardsDreams <66539660+TowardsDreams@users.noreply.github.com>
2021-02-26 21:14:55 -0600

    maths/枚举暴力

---
+++ user/codeforces/4A.Watermelon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	
+	int n;
+	cin>>n;
+	if(n==2) cout<<"NO"<<endl;
+	else if(n%2==0) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	
+	return 0;
+}
