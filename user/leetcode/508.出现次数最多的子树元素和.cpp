58e2eb207e8fb4e6a0b560b030257d8da3a0bfd0
dansen <370283593@qq.com>
2021-03-01 15:12:27 +0800

    add tree

---
+++ user/leetcode/508.出现次数最多的子树元素和.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=508 lang=cpp
+ *
+ * [508] 出现次数最多的子树元素和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/655.输出二叉树.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=655 lang=cpp
+ *
+ * [655] 输出二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<string>> printTree(TreeNode* root) {
+
+    }
+};
+// @lc code=end
+
