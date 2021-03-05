c2dd419227229b15bde4c6c27475b70c3c410d3b
dansen <370283593@qq.com>
2021-03-05 15:16:54 +0800

    add flask icon

--- src/leetcode_view.py
+++ src/leetcode_view.py
@@ -30,6 +30,15 @@ svg_text_key = '''
 </g>
 '''
 
+svg_icon_flask = '''
+<g transform="translate(%s, %s) scale(0.01)">
+  <symbol id="flask" viewBox="0 0 448 512">
+    <path d="M437.2 403.5L320 215V64h8c13.3 0 24-10.7 24-24V24c0-13.3-10.7-24-24-24H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h8v151L10.8 403.5C-18.5 450.6 15.3 512 70.9 512h306.2c55.7 0 89.4-61.5 60.1-108.5zM137.9 320l48.2-77.6c3.7-5.2 5.8-11.6 5.8-18.4V64h64v160c0 6.9 2.2 13.2 5.8 18.4l48.2 77.6h-172z"></path>
+  </symbol>
+  <use xlink:href="#flask"/>
+</g>
+'''
+
 class LeetcodeView:
     def __init__(self, leet):
         self.leet = leet
@@ -50,8 +59,10 @@ class LeetcodeView:
         p0 = points[0].split(",")
         x0 = float(p0[0])
         y0 = float(p0[1])
-        t = BeautifulSoup(svg_icon_finish % (str(x0-8), str(y0-6)), "xml").select_one("g")
-        n.append(t)
+        # add finish icon
+        if self.leet.check_finish(title):
+            t = BeautifulSoup(svg_icon_finish % (str(x0-8), str(y0-6)), "xml").select_one("g")
+            n.append(t)
         # key text
         p1 = points[1].split(",")
         x1 = float(p1[0])
@@ -63,6 +74,10 @@ class LeetcodeView:
         if 'key' in pro.tags:
             key_node = BeautifulSoup(svg_text_key % (str((x1+x0)/2), str(y2+5), pro.tags['key']), "xml").select_one("g")
             graph.append(key_node)
+        # add solution
+        if True:
+            t = BeautifulSoup(svg_icon_flask % (str(x0-8), str(y2-6)), "xml").select_one("g")
+            n.append(t)
 
     def leetcode_add_finish_icon(self, path):
         c = util.get_file_content(path)
