1f53e373883fe4484ebf5fc3b1e44638c056f4ec
dansen <370283593@qq.com>
2021-03-05 16:00:22 +0800

    update flask

--- src/leetcode_view.py
+++ src/leetcode_view.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 
 svg_icon_finish = '''
 <g transform="translate(%s, %s) scale(0.3)">
-<circle fill="#4CAF50" cx="24" cy="24" r="21"/>
-    <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
+<circle fill="#4caf50" cx="24" cy="24" r="21"/>
+    <polygon fill="#ccff90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
 </g>
 '''
 
@@ -19,7 +19,7 @@ svg_text_key = '''
 <g class='key'>
   <defs>
     <filter x="-0.1" y="-0.1" width="1.2" height="1.2" id="solid">
-      <feFlood flood-color="#CCFF90" result="bg" />
+      <feFlood flood-color="#ccff90" result="bg" />
       <feMerge>
         <feMergeNode in="bg"/>
         <feMergeNode in="SourceGraphic"/>
@@ -33,7 +33,7 @@ svg_text_key = '''
 svg_icon_flask = '''
 <g transform="translate(%s, %s) scale(0.01)">
   <symbol id="flask" viewBox="0 0 448 512">
-    <path d="M437.2 403.5L320 215V64h8c13.3 0 24-10.7 24-24V24c0-13.3-10.7-24-24-24H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h8v151L10.8 403.5C-18.5 450.6 15.3 512 70.9 512h306.2c55.7 0 89.4-61.5 60.1-108.5zM137.9 320l48.2-77.6c3.7-5.2 5.8-11.6 5.8-18.4V64h64v160c0 6.9 2.2 13.2 5.8 18.4l48.2 77.6h-172z"></path>
+    <path fill="#1389fd" d="M437.2 403.5L320 215V64h8c13.3 0 24-10.7 24-24V24c0-13.3-10.7-24-24-24H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h8v151L10.8 403.5C-18.5 450.6 15.3 512 70.9 512h306.2c55.7 0 89.4-61.5 60.1-108.5zM137.9 320l48.2-77.6c3.7-5.2 5.8-11.6 5.8-18.4V64h64v160c0 6.9 2.2 13.2 5.8 18.4l48.2 77.6h-172z"></path>
   </symbol>
   <use xlink:href="#flask"/>
 </g>
@@ -76,7 +76,7 @@ class LeetcodeView:
             graph.append(key_node)
         # add solution
         if True:
-            t = BeautifulSoup(svg_icon_flask % (str(x0-8), str(y2-6)), "xml").select_one("g")
+            t = BeautifulSoup(svg_icon_flask % (str(x0-13), str(y2-9)), "xml").select_one("g")
             n.append(t)
 
     def leetcode_add_finish_icon(self, path):
