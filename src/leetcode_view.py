9d5343cd525d860018c3c5987f419b5119aa610d
dansen <370283593@qq.com>
2021-03-05 11:02:07 +0800

    fix bug: key text not show

--- src/leetcode_view.py
+++ src/leetcode_view.py
@@ -43,15 +43,16 @@ class LeetcodeView:
 
     def post_process_problem_node(self, graph, n):
         title = n.title.get_text()
-        if not self.leet.check_finish(title):
-            return
+        
         # get positions
         points = n.g.polygon['points'].split()
         p0 = points[0].split(",")
         x0 = float(p0[0])
         y0 = float(p0[1])
         t = BeautifulSoup(svg_icon_finish % (str(x0-8), str(y0-6)), "xml").select_one("g")
-        n.append(t)
+        # add finish icon
+        if self.leet.check_finish(title):
+            n.append(t)
         # key text
         p1 = points[1].split(",")
         x1 = float(p1[0])
@@ -60,6 +61,7 @@ class LeetcodeView:
         x2 = float(p2[0])
         y2 = float(p2[1])
         pro = self.m.problem_map[title]
+        # add key text
         if 'key' in pro.tags:
             key_node = BeautifulSoup(svg_text_key % (str((x1+x0)/2), str(y2+5), pro.tags['key']), "xml").select_one("g")
             graph.append(key_node)
@@ -81,7 +83,8 @@ class LeetcodeView:
         self.leetcode_add_finish_icon(path)
 
     def generate_leetcode(self, leet, file, slug, out_name):
-        m = datamap.DataMap(util.get_file_content(util.get_map(file)))
+        c = util.get_file_content(util.get_map(file))
+        m = datamap.DataMap(c)
         self.m = m
         g = Digraph('stones', encoding='utf-8')
 
