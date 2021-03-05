b7e65ce24185ca36db4f4ce0f08990c6b8e50f02
dansen <370283593@qq.com>
2021-03-05 19:25:52 +0800

    Update 91.解码方法.cpp

--- user/leetcode/91.解码方法.cpp
+++ user/leetcode/91.解码方法.cpp
@@ -1,6 +1,7 @@
 /*
  * @lc app=leetcode.cn id=91 lang=cpp
- *
+ * 1210340156
+ * 
  * [91] 解码方法
  */
 
@@ -8,7 +9,19 @@
 class Solution {
 public:
     int numDecodings(string s) {
-
+        int n = s.length();
+        int f[n+1];
+        f[0] = 1;
+        for(int i=2; i<=n; i++){
+            // single
+            if(s[i-1] != '0'){
+                f[i] += f[i-1];
+            }
+            
+            // double
+            f[i] += f[i-2];
+        }
+        return 0;
     }
 };
 // @lc code=end
