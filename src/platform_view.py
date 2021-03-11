aee0dfd21b497b9f45644b7f0af504fd436b7268
dansen <370283593@qq.com>
2021-03-11 11:04:03 +0800

    svg icon bug

--- src/platform_view.py
+++ src/platform_view.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 
 svg_icon_finish = '''
 <g transform="translate(%s, %s) scale(0.3)">
-<circle fill="#4caf50" cx="24" cy="24" r="21"/>
+    <circle fill="#4caf50" cx="24" cy="24" r="21"/>
     <polygon fill="#ccff90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
 </g>
 '''
@@ -31,12 +31,12 @@ svg_text_key = '''
 '''
 
 svg_icon_flask = '''
-<g transform="translate(%s, %s) scale(0.01)">
+<g transform="translate(%s, %s)">
   <symbol id="flask" viewBox="0 0 448 512">
     <path fill="#1389fd" d="M437.2 403.5L320 215V64h8c13.3 0 24-10.7 24-24V24c0-13.3-10.7-24-24-24H120c-13.3 0-24 10.7-24 24v16c0 13.3 10.7 24 24 24h8v151L10.8 403.5C-18.5 450.6 15.3 512 70.9 512h306.2c55.7 0 89.4-61.5 60.1-108.5zM137.9 320l48.2-77.6c3.7-5.2 5.8-11.6 5.8-18.4V64h64v160c0 6.9 2.2 13.2 5.8 18.4l48.2 77.6h-172z"></path>
   </symbol>
   <a href="%s">
-  <use xlink:href="#flask"/>
+  <use xlink:href="#flask" width="18" height="18"/>
   </a>
 </g>
 '''
@@ -117,6 +117,6 @@ class PlatformView(object):
         flask = self.check_flask(title)
         if flask != "":
             url = "https://github.com/acm-clan/algorithm-stone/blob/main/user/%s/%s" % (self.slug, flask)
-            text = svg_icon_flask % (str(x0-13), str(y2-9), url)
+            text = svg_icon_flask % (str(x0-9), str(y2-9), url)
             t = BeautifulSoup(text, "xml").select_one("g")
             n.append(t)
