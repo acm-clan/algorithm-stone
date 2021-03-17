dfdf5e60cfa91a427287a51c92a62dfa79b33683
dansen <370283593@qq.com>
2021-03-17 15:16:26 +0800

    add queue

--- user/leetcode/1642.可以到达的最远建筑.cpp
+++ user/leetcode/1642.可以到达的最远建筑.cpp
@@ -8,7 +8,26 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-
+        int n = heights.size();
+        // 由于我们需要维护最大的 l 个值，因此使用小根堆
+        priority_queue<int, vector<int>, greater<int>> q;
+        // 需要使用砖块的 delta h 的和
+        int s = 0;
+        for (int i = 1; i < n; ++i) {
+            int d = heights[i] - heights[i - 1];
+            if (d > 0) {
+                q.push(d);
+                // 如果优先队列已满，需要拿出一个其中的最小值，改为使用砖块
+                if (q.size() > ladders) {
+                    s += q.top();
+                    q.pop();
+                }
+                if (s > bricks) {
+                    return i - 1;
+                }
+            }
+        }
+        return n - 1;
     }
 };
 // @lc code=end
--- user/leetcode/1753.移除石子的最大得分.cpp
+++ user/leetcode/1753.移除石子的最大得分.cpp
@@ -8,7 +8,26 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-
+        priority_queue<int> q;
+        int ans = 0;
+        q.push(a);
+        q.push(b);
+        q.push(c);
+        while(true){
+            int max_num = q.top();
+            q.pop();
+            int medium = q.top();
+            q.pop();
+            int min_num = q.top();
+            if(!medium)break; //结束条件
+            int temp = max(medium - min_num, 1); //我要从最多的两堆里拿多少
+            max_num -= temp;
+            medium -= temp;
+            ans += temp; //更新答案
+            q.push(max_num);
+            q.push(medium);
+        }
+        return ans;
     }
 };
 // @lc code=end
