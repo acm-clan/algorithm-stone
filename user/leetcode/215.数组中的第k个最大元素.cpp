e1993508d4241cb6a4a749bba4c3a406773c3fb2
dansen <370283593@qq.com>
2021-03-19 14:55:45 +0800

    堆与第K大元素

--- user/leetcode/215.数组中的第k个最大元素.cpp
+++ user/leetcode/215.数组中的第k个最大元素.cpp
@@ -3,20 +3,22 @@
  *
  * [215] 数组中的第K个最大元素
  */
+#include <queue>
+using namespace std;
 
 // @lc code=start
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (int i = 0; i < nums.size(); i++) {
-            pq.push(nums[i]);
-            if (pq.size() > k) {
-                pq.pop();
+        // 用小堆
+        priority_queue<int, vector<int>, greater<int>> q;
+        for(auto v : nums){
+            q.push(v);
+            if(q.size()>k){
+                q.pop();
             }
         }
-
-        return pq.top();
+        return q.top();
     }
 };
 // @lc code=end
---
+++ user/leetcode/71.简化路径.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=cpp
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string simplifyPath(string path) {
+
+    }
+};
+// @lc code=end
+
