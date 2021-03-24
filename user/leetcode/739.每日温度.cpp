6d4b7e869661d9321fb19c4fcd5b959eff296924
acm-clan <dansen007@gmail.com>
2021-03-24 22:05:31 +0800

    add 739 draw.io

--- user/leetcode/739.每日温度.cpp
+++ user/leetcode/739.每日温度.cpp
@@ -4,12 +4,38 @@
  * [739] 每日温度
  */
 
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
 // @lc code=start
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
+    vector<int> dailyTemperatures(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> res(n, 0);
+        // 栈里面存放当前的索引位置
+        stack<int> st;
 
+        for (int i = 0; i < arr.size(); ++i) {
+            // 如果当前元素大于栈顶元素，说明需要让栈顶出栈
+            while (!st.empty() && arr[i] > arr[st.top()]) {
+                auto t = st.top();
+                st.pop();
+                // 距离就是当前位置
+                res[t] = i - t;
+            }
+            st.push(i);
+        }
+        return res;
     }
 };
 // @lc code=end
-
