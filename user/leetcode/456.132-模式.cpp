730586246308fa79f7fca3f2b43c2ee2a7014080
dansen <370283593@qq.com>
2021-03-24 19:19:33 +0800

    update

---
+++ user/leetcode/456.132-模式.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=456 lang=cpp
+ *
+ * [456] 132模式
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/636.函数的独占时间.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=636 lang=cpp
+ *
+ * [636] 函数的独占时间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+
+    }
+};
+// @lc code=end
+
