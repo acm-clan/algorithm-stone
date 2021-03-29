d9e47f704ed38e29176c943b389255900c8b0281
acm-clan <dansen007@gmail.com>
2021-03-29 08:13:59 +0800

    update stack

---
+++ user/leetcode/1003.检查替换后的词是否有效.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1003 lang=cpp
+ *
+ * [1003] 检查替换后的词是否有效
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+
+    }
+};
+// @lc code=end
+
