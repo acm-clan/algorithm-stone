254c6f6826f618ccad8d903ba4c8a2fcdeae4522
dansen <370283593@qq.com>
2021-03-30 16:27:15 +0800

    add stack

---
+++ user/leetcode/1209.删除字符串中的所有相邻重复项-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1209 lang=cpp
+ *
+ * [1209] 删除字符串中的所有相邻重复项 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/1249.移除无效的括号.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1249 lang=cpp
+ *
+ * [1249] 移除无效的括号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+
+    }
+};
+// @lc code=end
+
