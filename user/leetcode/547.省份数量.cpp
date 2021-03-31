3536dd75187687cb8a31ca02e48bad37f83d40f3
dansen <370283593@qq.com>
2021-03-31 20:47:39 +0800

    add union find

--- user/leetcode/547.省份数量.cpp
+++ user/leetcode/547.省份数量.cpp
@@ -13,33 +13,34 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int Find(vector<int>& parent, int index) {
-        if (parent[index] != index) {
-            parent[index] = Find(parent, parent[index]);
+    int Find(vector<int>& group, int index) {
+        if (group[index] != index) {
+            group[index] = Find(group, group[index]);
         }
-        return parent[index];
+        return group[index];
     }
 
-    void Union(vector<int>& parent, int index1, int index2) {
-        parent[Find(parent, index1)] = Find(parent, index2);
+    void Union(vector<int>& group, int index1, int index2) {
+        group[Find(group, index1)] = Find(group, index2);
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int provinces = isConnected.size();
-        vector<int> parent(provinces);
-        for (int i = 0; i < provinces; i++) {
-            parent[i] = i;
+        int count = isConnected.size();
+        vector<int> group(count);
+
+        for (int i = 0; i < count; i++) {
+            group[i] = i;
         }
-        for (int i = 0; i < provinces; i++) {
-            for (int j = i + 1; j < provinces; j++) {
+        for (int i = 0; i < count; i++) {
+            for (int j = i + 1; j < count; j++) {
                 if (isConnected[i][j] == 1) {
-                    Union(parent, i, j);
+                    Union(group, i, j);
                 }
             }
         }
         int circles = 0;
-        for (int i = 0; i < provinces; i++) {
-            if (parent[i] == i) {
+        for (int i = 0; i < count; i++) {
+            if (group[i] == i) {
                 circles++;
             }
         }
