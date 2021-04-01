a78caf553dd5c9a3eef7cf839bf158b8ef222b82
acm-clan <dansen007@gmail.com>
2021-04-02 07:48:57 +0800

    create logo

--- animations/test_scene.py
+++ animations/test_scene.py
@@ -3,7 +3,7 @@ from manim_imports_ext import *
 
 class TestScene(AlgoScene):
     def construct(self):
-        c = AlgoLogo(fill_color=BLUE, fill_opacity = 1).scale(0.5)
-        self.add(c)
-
+        self.wait(0.2)
+        logo = AlgoLogo().scale(0.2)
+        self.play(ShowCreation(logo))
         self.wait(2)
