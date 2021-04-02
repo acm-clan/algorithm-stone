685d87b460779fc0b93a7dae79ce5af70bcfb0b7
acm-clan <dansen007@gmail.com>
2021-04-02 08:11:33 +0800

    add start animation

--- animations/test_scene.py
+++ animations/test_scene.py
@@ -4,6 +4,5 @@ from manim_imports_ext import *
 class TestScene(AlgoScene):
     def construct(self):
         self.wait(0.2)
-        logo = AlgoLogo().scale(0.2)
-        self.play(ShowCreation(logo))
+        self.start_logo()
         self.wait(2)
