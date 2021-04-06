c376a32f6bd57dae3cb87edcafc24b866b61f269
dansen <370283593@qq.com>
2021-04-06 18:49:55 +0800

    add segment tree

---
+++ user/leetcode/307.区域和检索-数组可修改.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=307 lang=cpp
+ *
+ * [307] 区域和检索 - 数组可修改
+ */
+
+// @lc code=start
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+
+    }
+    
+    void update(int index, int val) {
+
+    }
+    
+    int sumRange(int left, int right) {
+
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
+// @lc code=end
+
