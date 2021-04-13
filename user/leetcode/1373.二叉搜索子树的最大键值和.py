f5d21d82fd043cc8349dcd5181231c5455c56364
dansen <370283593@qq.com>
2021-04-13 11:55:18 +0800

    bst

---
+++ user/leetcode/1373.二叉搜索子树的最大键值和.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1373 lang=python3
+#
+# [1373] 二叉搜索子树的最大键值和
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxSumBST(self, root: TreeNode) -> int:
+        
+# @lc code=end
+
