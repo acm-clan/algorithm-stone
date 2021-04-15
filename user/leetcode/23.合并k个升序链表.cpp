036ab3a1cd815a1354e388dd40839c31d8cbab58
chocolate-emperor <654628081@example.com>
2021-04-15 10:49:43 +0800

    添加链表题 23

--- user/leetcode/23.合并k个升序链表.cpp
+++ user/leetcode/23.合并k个升序链表.cpp
@@ -17,8 +17,42 @@
  */
 class Solution {
 public:
+    struct cmp{
+        bool operator()(ListNode*a,ListNode*b){
+            return a->val >b->val;
+        }
+    };
+    priority_queue<ListNode*,vector<ListNode*>,cmp >pq;
+    unordered_map<ListNode*,int>node2index;
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode*sentry=new ListNode();
+        if(lists.size()==0)  return nullptr;
+        
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]){
+                pq.push(lists[i]);
+                node2index[lists[i]]=i;
+                lists[i] = lists[i]->next;
+            }
+        }
 
+        ListNode*curr=sentry;
+        while(!pq.empty()){
+            ListNode*tmp=pq.top();
+            pq.pop();
+            curr->next =tmp;
+            curr=curr->next;
+            int i = node2index[tmp];
+            if(lists[i])  {
+                pq.push(lists[i]);
+                node2index[lists[i]]=i;
+                lists[i]=lists[i]->next;
+            }
+        }
+        curr->next=nullptr;
+        ListNode*res=sentry->next;
+        delete sentry;
+        return res;
     }
 };
 // @lc code=end
