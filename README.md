17077de6b9394bf269011d6e01b82c871707c79d
dansen <370283593@qq.com>
2021-05-06 16:08:39 +0800

    add leetcode 220

--- README.md
+++ README.md
@@ -88,7 +88,7 @@
 |[最小最大值](https://leetcode-cn.com/tag/minimax)| `minimax`  | 7 |`dansen` | 100% |
 |[队列](https://leetcode-cn.com/tag/queue)| queue  | 10 |`dansen` | 100% |
 |[几何题](https://leetcode-cn.com/tag/geometry)| geometry  | 12 | `dansen`| 100% |
-|[排序Map](https://leetcode-cn.com/tag/ordered-map)| ordered-map  | 13 | | 0% |
+|[排序Map](https://leetcode-cn.com/tag/ordered-map)| ordered-map  | 13 |`dansen` | 0% |
 |[线段树](https://leetcode-cn.com/tag/segment-tree)| segment-tree  | 15 | | 0% |
 |[Trie树](https://leetcode-cn.com/tag/trie)| trie  | 17 | | 0% |
 |[滑动窗口](https://leetcode-cn.com/tag/sliding-window)| sliding-window  | 25 |`dansen` | 100% |
---
+++ user/leetcode/220.存在重复元素-iii.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=220 lang=cpp
+ *
+ * [220] 存在重复元素 III
+ */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& n, int k, int t)
+    {
+        set<long> st;
+        for (int i = 0; i < n.size(); i++) {
+            // 找一个在区间n[i]-t到n[i]+t的数
+            // 查找大于等于n[i]-t的值
+            auto lb = st.lower_bound((long)n[i] - t);
+            // 必须小于n[i]+t的值
+            if (lb != st.end() && *lb <= (long)n[i] + t)
+                return 1;
+            st.insert(n[i]);
+            if (i >= k)
+                st.erase(n[i - k]);
+        }
+        return 0;
+    }
+};
+// @lc code=end
