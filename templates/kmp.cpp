ba1f45fd752b1d59c4737027baf1b105f81dacfb
dansen <370283593@qq.com>
2021-05-18 13:18:47 +0800

    add kmp

--- templates/kmp.cpp
+++ templates/kmp.cpp
@@ -10,56 +10,61 @@ using namespace std;
 
 std::vector<int> compute_prefix_function(std::string & p)
 {
-    int m = p.length();
-    std::vector<int> next(m);
+    int n = p.length();
+    std::vector<int> next(n);
 
     next[0] = -1;
-    int i = 0;
-    int j = -1;
+    int j = 0;
+    int k = -1;
 
-    while(i<m){
-        if(j == -1 || p[i] == p[j]){
-            i++;
+    while(j<n-1){
+        if(k == -1 || p[k] == p[j]){
+            k++;
             j++;
-            next[i] = j;
+            next[j] = k;
         }else{
-            j = next[j];
+            k = next[k];
         }
     }
-
-    for(auto i : next){
-        printf("%d ", i);
-    }
-    printf("\n");
     
     return next;
+}
+
+void print(std::vector<int> v)
+{
+    for(auto i : v){
+        printf("%d ",i);
+    }
+    printf("\n");
 }
 
 int kmp_matcher(std::string & t, std::string & p)
 {
     int n = t.length();
     int m = p.length();
-    printf("n %d m %d\n", n, m);
     auto pi = compute_prefix_function(p);
-    // int q = 0;
-    // for(int i=0; i<n;){
-    //     while(q>0 && p[q+1]!=t[i]){
-    //         q = pi[q];
-    //     }
-    //     if(pi[q+1] == t[i]){
-    //         q++;
-    //     }
-    //     if(q == m){
-    //         return i-m;
-    //     }
-    // }
-    return 0;
+
+    int j = 0;
+    int k = 0;
+
+    while(k<m && j<n){
+        if(k == -1 || t[j] == p[k]){
+            k++;
+            j++;
+        }else{
+            k = pi[k];
+        }
+    }
+    if(k == m){
+        return j-m;
+    }
+    return -1;
 }
 
 int main()
 {
     std::string t = "ababcabaa";
-    std::string p = "ababcabaa";
+    std::string p = "bca";
     int pos = kmp_matcher(t, p);
     printf("pos:%d\n", pos);
     return 0;
---
+++ templates/kmp.py
@@ -0,0 +1,61 @@
+import numpy as np
+import random
+import string
+
+def compute_prefix_function(p, back=False):
+    n = len(p)
+    next = np.zeros(n, dtype=int)
+    k = -1
+    j = 0
+    next[0] = -1
+    while j < n-1:
+        if k == -1 or p[j] == p[k]:
+            k+=1
+            j+=1
+            next[j] = k
+            if back:
+                print("next[%s]=%s"%(str(j), str(k)))
+        else:
+            if back:
+                print("back:", p[j], p[k], k, "->", next[k])
+            k = next[k]
+    return next
+
+def check_next(next):
+    last = -2
+    for i in next:
+        if last == -2:
+            last = i
+        else:
+            if i < last and i >=2:
+                print(next)
+                return True
+            last = i
+    return False
+
+def run():
+    random.seed(1)
+    for _ in range(100):
+        p = ''.join(random.choices("abc", k=15))
+        next = compute_prefix_function(p)
+        if check_next(next):
+            print(p)
+
+def run2():
+    p = "ababbcaababac"
+    print(p)
+    next = compute_prefix_function(p, back=True)
+    print(next)
+
+# ababbcaababaccc
+# ababa 内部的状态
+# b的内部状态是2
+# 状态的嵌套
+# 塌缩：在当前前缀中寻找新的较小的状态值
+# 很像动态规划利用已有的信息
+# 自底向上进行计算
+# next里面的值是表示
+# 
+
+if __name__ == "__main__":
+    run2()
