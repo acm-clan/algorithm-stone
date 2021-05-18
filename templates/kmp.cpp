b69a30b41fe68985ebe1c693fcb4b68adedeea9b
dansen <370283593@qq.com>
2021-05-18 13:23:05 +0800

    Update kmp.cpp

--- templates/kmp.cpp
+++ templates/kmp.cpp
@@ -1,14 +1,14 @@
-#include <queue>
-#include <map>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
 #include <stdlib.h>
 #include <string.h>
-#include <stack>
-#include <unordered_set>
 #include <string>
+#include <unordered_set>
 using namespace std;
 
-std::vector<int> compute_prefix_function(std::string & p)
+std::vector<int> compute_prefix_function(std::string& p)
 {
     int n = p.length();
     std::vector<int> next(n);
@@ -17,46 +17,45 @@ std::vector<int> compute_prefix_function(std::string & p)
     int j = 0;
     int k = -1;
 
-    while(j<n-1){
-        if(k == -1 || p[k] == p[j]){
+    while (j < n - 1) {
+        if (k == -1 || p[k] == p[j]) {
             k++;
             j++;
             next[j] = k;
-        }else{
+        } else {
             k = next[k];
         }
     }
-    
+
     return next;
 }
 
 void print(std::vector<int> v)
 {
-    for(auto i : v){
-        printf("%d ",i);
+    for (auto i : v) {
+        printf("%d ", i);
     }
     printf("\n");
 }
 
-int kmp_matcher(std::string & t, std::string & p)
+int kmp_matcher(std::string& t, std::string& p)
 {
-    int n = t.length();
-    int m = p.length();
-    auto pi = compute_prefix_function(p);
+    auto next = compute_prefix_function(p);
 
     int j = 0;
     int k = 0;
 
-    while(k<m && j<n){
-        if(k == -1 || t[j] == p[k]){
+    while (j < t.length() && k < p.length()) {
+        if (k == -1 || t[j] == p[k]) {
             k++;
             j++;
-        }else{
-            k = pi[k];
+        } else {
+            k = next[k];
         }
     }
-    if(k == m){
-        return j-m;
+
+    if (k == p.length()) {
+        return j - p.length();
     }
     return -1;
 }
