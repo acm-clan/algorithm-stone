8a02ca974c25696cb725b825abb88c7ab3efa003
dansen <370283593@qq.com>
2021-06-03 15:19:04 +0800

    add 211

--- user/leetcode/211.添加与搜索单词-数据结构设计.cpp
+++ user/leetcode/211.添加与搜索单词-数据结构设计.cpp
@@ -3,73 +3,62 @@
  *
  * [211] 添加与搜索单词 - 数据结构设计
  */
+#include <iostream>
 #include <vector>
-#include <map>
-#include <string>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <string.h>
 using namespace std;
-// @lc code=start
-class WordDictionary {
-    class Trie {
-public:
-    Trie* c [28];
-    char v :1;
-    /** Initialize your data structure here. */
-    Trie() {
-        memset(c, 0, sizeof(c));
-        v = 0;
-    }
 
-    Trie* searchPrefix(string & prefix) {
-        Trie* p = this;
-        for(char &ch : prefix){
-            char index = ch-'a';
-            if(ch == '.')index=26;
-            if(!p->c[index]){
-                return 0;
-            }
-            p = p->c[index];
-        }
-        return p;
-    }
-    
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        Trie* p = this;
-        for(char &ch:word){
-            char index = ch-'a';
-            if(ch == '.')index=26;
-            if(p->c[index] == 0){
-                p->c[index] = new Trie();
-            }
-            p = p->c[index];
-        }
-        p->v = 1;
-    }
-    
-    /** Returns if the word is in the trie. */
-    bool search(string word) {
-        Trie* v = searchPrefix(word);
-        return v && v->v;
-    }
-    
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
-        return searchPrefix(prefix);
+// @lc code=start
+struct Node {
+    bool isword;
+    Node* children[26];
+    Node() {
+        isword = false;
+        for (auto& child : children)
+            child = nullptr;
     }
 };
+
+
+class WordDictionary {
+private:
+    Node* root;
 public:
-    Trie t;
     /** Initialize your data structure here. */
     WordDictionary() {
+        root = new Node();
+    }
 
-    }
-    
     void addWord(string word) {
-        t.insert(word);
+        Node* p = root;
+        for (char& ch : word) {
+            if (!p->children[ch - 'a'])
+                p->children[ch - 'a'] = new Node();
+            p = p->children[ch - 'a'];
+        }
+        p->isword = true;
     }
-    
+
     bool search(string word) {
-        return t.search(word);
+        return _search(word, root, 0);
+    }
+
+    bool _search(string& word, Node* p, int i) {
+        if (!p) return false;
+
+        if (i == word.size()) return p->isword;
+
+        if (word[i] == '.') {
+            for (int j = 0; j < 26; ++j) {
+                if (_search(word, p->children[j], i+1))
+                    return true;
+            }
+            return false;
+        }
+        return _search(word, p->children[word[i] - 'a'], i+1);
     }
 };
 
@@ -80,4 +69,3 @@ public:
  * bool param_2 = obj->search(word);
  */
 // @lc code=end
-
