58ce8b50ac58664b1ceea5f7dbe9c04522867809
dansen <370283593@qq.com>
2021-06-04 19:22:19 +0800

    add 648

--- user/leetcode/648.单词替换.cpp
+++ user/leetcode/648.单词替换.cpp
@@ -3,12 +3,98 @@
  *
  * [648] 单词替换
  */
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+using namespace std;
 
 // @lc code=start
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
+    class Trie{
+    private:
+        Trie * c[26];
+        bool end=false;
+    public:
+        Trie(){
+            memset(c, 0, sizeof(c));
+        }
 
+        void insert(string v){
+            auto p = this;
+            for(auto c : v){
+                int i = c - 'a';
+                if(!p->c[i]){
+                    p->c[i] = new Trie();
+                }
+                p = p->c[i];
+            }
+            p->end = true;
+        }
+
+        void print(Trie * t){
+            if(!t)return;
+            
+            for(int i=0; i<26; i++){
+                if(t->c[i]){
+                    printf("%c ", i+'a');
+                    print(t->c[i]);
+                    printf("\n");
+                }
+            }
+        }
+
+        void get_short(Trie * t, string & word, int i, string & con, bool & find){
+            if(i >= word.length()){
+                return;
+            }
+            auto index = word[i]-'a';
+            if(!t->c[index]){
+                return;
+            }
+            if(t->c[index]->end){
+                con += word[i];
+                find = true;
+                return;
+            }else{
+                con += word[i];
+                get_short(t->c[index], word, i+1, con, find);
+            }
+        }
+
+        string get(string word){
+            string con;
+            int i=0;
+            bool find = false;
+            get_short(this, word, i, con, find);
+            if(!find){
+                con = word;
+            }
+            //printf("get word %s %s\n", word.c_str(), con.c_str());
+            return con;
+        }
+    };
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        Trie node;
+        for(auto v : dictionary){
+            node.insert(v);
+        }
+        //node.print(&node);
+
+        string ans;
+        string w;
+        for(char c : sentence){
+            if(c == ' '){
+                ans += node.get(w)+" ";
+                w = "";
+            }else{
+                w += c;
+            }
+        }
+        ans += node.get(w);
+        return ans;
     }
 };
 // @lc code=end
---
+++ user/leetcode/676.实现一个魔法字典.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=676 lang=cpp
+ *
+ * [676] 实现一个魔法字典
+ */
+
+// @lc code=start
+class MagicDictionary {
+public:
+    /** Initialize your data structure here. */
+    MagicDictionary() {
+
+    }
+    
+    void buildDict(vector<string> dictionary) {
+
+    }
+    
+    bool search(string searchWord) {
+
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
+// @lc code=end
+
