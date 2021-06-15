a6c7d985821def6b066a654733dd037f859b2a25
dansen <370283593@qq.com>
2021-06-15 19:15:07 +0800

    add binary search

---
+++ user/leetcode/167.两数之和-ii-输入有序数组.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=cpp
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/1670.设计前中后队列.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=1670 lang=cpp
+ *
+ * [1670] 设计前中后队列
+ */
+
+// @lc code=start
+class FrontMiddleBackQueue {
+public:
+    FrontMiddleBackQueue() {
+
+    }
+    
+    void pushFront(int val) {
+
+    }
+    
+    void pushMiddle(int val) {
+
+    }
+    
+    void pushBack(int val) {
+
+    }
+    
+    int popFront() {
+
+    }
+    
+    int popMiddle() {
+
+    }
+    
+    int popBack() {
+
+    }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
+// @lc code=end
+
