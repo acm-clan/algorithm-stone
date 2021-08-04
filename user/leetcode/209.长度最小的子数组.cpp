c3acaeabe1a32b8d988231dfddbde82e045b5058
dansen <370283593@qq.com>
2021-08-04 16:34:01 +0800

    binary search

---
+++ user/leetcode/209.长度最小的子数组.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=cpp
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/240.搜索二维矩阵-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=cpp
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+    }
+};
+// @lc code=end
+
