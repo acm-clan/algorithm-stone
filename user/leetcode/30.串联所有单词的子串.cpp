12e46463bb57929dfb4ab142f5cf0e8e69d460a8
dansen <370283593@qq.com>
2021-08-23 18:40:56 +0800

    add string

---
+++ user/leetcode/30.串联所有单词的子串.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=30 lang=cpp
+ *
+ * [30] 串联所有单词的子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+    }
+};
+// @lc code=end
+
