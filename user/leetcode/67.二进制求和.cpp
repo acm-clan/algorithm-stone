988b424d0162700746a29271ee96a5db7d472466
dansen <370283593@qq.com>
2021-11-03 22:23:27 +0800

    update string

---
+++ user/leetcode/67.二进制求和.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=67 lang=cpp
+ *
+ * [67] 二进制求和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+    }
+};
+// @lc code=end
+
