56e87b994aea9f96abe37fee481b345eb4461e0a
dansen <370283593@qq.com>
2021-12-28 17:34:43 +0800

    update string

---
+++ user/leetcode/541.反转字符串-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=cpp
+ *
+ * [541] 反转字符串 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+
+    }
+};
+// @lc code=end
+
---
+++ user/leetcode/551.学生出勤记录-i.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=551 lang=cpp
+ *
+ * [551] 学生出勤记录 I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool checkRecord(string s) {
+
+    }
+};
+// @lc code=end
+
