eb33508feb0a08d4d78b75223af5c574a2d0dd81
dansen <370283593@qq.com>
2022-01-13 10:12:11 +0800

    update string

---
+++ user/leetcode/936.戳印序列.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=936 lang=cpp
+ *
+ * [936] 戳印序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> movesToStamp(string stamp, string target) {
+
+    }
+};
+// @lc code=end
+
