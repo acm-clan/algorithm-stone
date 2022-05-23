89324e8e79b517b7b476200b203bff8648ce09f7
dansen <370283593@qq.com>
2022-05-23 17:35:03 +0800

    update string

---
+++ user/leetcode/932.漂亮数组.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=932 lang=cpp
+ *
+ * [932] 漂亮数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+
+    }
+};
+// @lc code=end
+
